ede7910db206c0e8c064e9302282fff1bb74061f
Kgothatso Mahloko <kmakhuba@c5r10s8.wethinkcode.co.za>
2020-06-23 15:23:07 +0200

    *

---
+++ ex04/ft_print_comb.c
@@ -0,0 +1,50 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_comb.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kmakhuba <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/06/23 15:13:09 by kmakhuba          #+#    #+#             */
+/*   Updated: 2020/06/23 15:16:44 by kmakhuba         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+void	ft_putchar(char c);
+
+void	ft_print_3_char(char a, char b, char c)
+{
+	ft_putchar(a);
+	ft_putchar(b);
+	ft_putchar(c);
+	if (a < '7')
+	{
+		ft_putchar(',');
+		ft_putchar(' ');
+	}
+}
+
+void	ft_print_comb(void)
+{
+	char a;
+	char b;
+	char c;
+
+	a = '0';
+	while (a <= '9')
+	{
+		b = a + 1;
+		while (b <= '9')
+		{
+			c = b + 1;
+			while (c <= '9')
+			{
+				ft_print_3_char(a, b, c);
+				c++;
+			}
+			b++;
+		}
+		a++;
+	}
+}
+
