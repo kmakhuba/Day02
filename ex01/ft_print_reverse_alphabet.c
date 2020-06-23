b067d40ececb784b6dc548ed2f8bac2ab2884697
Kgothatso Mahloko <kmakhuba@c5r10s8.wethinkcode.co.za>
2020-06-23 11:46:10 +0200

    *

--- ex01/ft_print_reverse_alphabet.c
+++ ex01/ft_print_reverse_alphabet.c
@@ -6,17 +6,9 @@
 /*   By: kmakhuba <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/06/23 11:15:15 by kmakhuba          #+#    #+#             */
-/*   Updated: 2020/06/23 11:42:24 by kmakhuba         ###   ########.fr       */
+/*   Updated: 2020/06/23 11:45:53 by kmakhuba         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-#include <unistd.h>
-
-void ft_putchar (char c)
-
-{
-	write(1, &c, 1);
-
-}
 void 	ft_print_reverse_alphabet(void)
 {
 	char ch;
@@ -28,8 +20,4 @@ void 	ft_print_reverse_alphabet(void)
 		ch--;
 	}
 }
-int main ()
-{
-	ft_print_reverse_alphabet();
-	return 0;
-}
+
